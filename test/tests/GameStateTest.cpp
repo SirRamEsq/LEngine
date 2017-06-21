a427a3e45d2d0c3e24433dbbaf1d95bef365aed1
SirRamESQ <gmrl95@gmail.com>
2017-06-21 15:01:04 -0400

    Added Catch.hpp and Unit Tests

---
+++ test/tests/GameStateTest.cpp
@@ -0,0 +1,18 @@
+#include "catch.hpp"
+#include "../../Source/StateManager.h"
+#include "../../Source/Kernel.h"
+/*
+class _test_State : private GameState{
+
+};
+
+TEST_CASE( "Can Load State", "[state]" ) {
+    Kernel::Inst();
+
+
+//    K_StateMan.PushState(make_unique<_test_State>(&stateMan));
+
+    Kernel::Close();
+}
+*/
+
