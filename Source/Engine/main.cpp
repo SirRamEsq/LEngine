6b3b7090c9625027baf7c459acab29b05dbcc203
SirRamESQ <gmrl95@gmail.com>
2017-09-01 11:34:28 -0400

    Added documentation

--- Source/Engine/main.cpp
+++ Source/Engine/main.cpp
@@ -13,3 +13,18 @@ int main(int argc, char *argv[]){
 
 	return 0;
 }
+
+/*! \mainpage LEngine
+ *
+ * \section intro_sec Introduction
+ *
+ * 2D Game Engine!
+ *
+ * \section script_documentation Scripting Documentation
+ *
+ * \subsection LuaInterface Lua Interface
+ * 		Any Engine function in lua can be accessed by CPP.interface:<funcName> <br>
+ * 		LuaInterface functions can be found at LuaInterface::ExposeCPP <br>
+ * 		if the engine returns a C++ type, you can reference the Expose Interface function to see what functions that data type has exposed
+ * 		\see LuaInterface::ExposeCPP()
+ */
