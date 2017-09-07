72c96b62beffab7c3a1d15a9373effa2d575c805
SirRamESQ <gmrl95@gmail.com>
2017-09-07 09:47:13 -0400

    Minor additions

--- valgrind.sh
+++ valgrind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Run valgrind, send standard output and errors into file
-valgrind --leak-check=full -v --track-origins=yes ./bin/LEngine > VALGRINDOutput.txt 2>&1
+valgrind --tool=memcheck --leak-check=full -v --track-origins=yes ./bin/LEngine > VALGRINDOutput.txt 2>&1
 #valgrind --leak-check=summary -v --track-origins=yes ./bin/LEngine > VALGRINDOutput.txt 2>&1
 
