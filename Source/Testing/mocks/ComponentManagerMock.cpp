c0ad8ccafa55aee5535f067ce1e3cee6f7834b82
SirRamESQ <gmrl95@gmail.com>
2017-09-22 15:27:03 -0400

    Unit tests again pass

--- Source/Testing/mocks/ComponentManagerMock.cpp
+++ Source/Testing/mocks/ComponentManagerMock.cpp
@@ -1,6 +1,6 @@
 #include "ComponentManagerMock.h"
 
-ComponentTest::ComponentTest(EID id, const std::string& logName) : BaseComponent(id, logName){
+ComponentTest::ComponentTest(EID id, ComponentTestManager* manager) : BaseComponent(id, manager){
 	updateCount = 0;
 }
 
@@ -23,14 +23,13 @@ void ComponentTest::HandleEvent(const Event* event){
 ////////////////////////
 //ComponentTestManager//
 ////////////////////////
-ComponentTestManager::ComponentTestManager(EventDispatcher* e) : BaseComponentManager("LOGTEST", e){
+ComponentTestManager::ComponentTestManager(EventDispatcher* e) : BaseComponentManager_Impl(e){
 
 }
 
-void ComponentTestManager::AddComponent(EID id, EID parent){
-    auto i = componentList.find(id);
-    if(i!=componentList.end()){return;}
-    auto sprite = make_unique<ComponentTest>(id, logFileName);
-    componentList[id] = std::move(sprite);
+std::unique_ptr<ComponentTest> ComponentTestManager::ConstructComponent(EID id, ComponentTest* parent){
+    auto component = make_unique<ComponentTest>(id, this);
+	component->SetParent(parent);
+    return std::move(component);
 }
 
