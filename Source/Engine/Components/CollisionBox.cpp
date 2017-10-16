ce8b321531f31f500f0872f3a0d67268a6c4d236
SirRamESQ <gmrl95@gmail.com>
2017-10-16 11:53:02 -0400

    ComponentCollision uses New CollisionBox; Tests pass
    Removed ALWAYS_CHECK flag for collision boxes. Has been ignored for some time (since buckets introduced) and are now removed
    Can use any shape with collision boxes.

--- Source/Engine/Components/CollisionBox.cpp
+++ Source/Engine/Components/CollisionBox.cpp
@@ -1,10 +1,18 @@
 #include "CollisionBox.h"
-#include "../CompPosition.h"
+#include "CompPosition.h"
 
-CollisionBox::CollisionBox(unsigned int id, int order, uint8_t flags, std::unique_ptr<Shape>* shape, ComponentPosition* pos)
-	: mId(id), mOrder(order), mFlags(flags), mShape(std::move(*shape)), mPos(pos){
+CollisionBox::CollisionBox(unsigned int id, int order, uint8_t flags, const Shape& shape, ComponentPosition* pos)
+	: mId(id), mOrder(order), mFlags(flags), mPos(pos){
 
+	mActive = true;
 	ASSERT( mShape.get() != NULL);
+
+	SetShape(&shape);
+}
+
+void CollisionBox::SetShape(const Shape* shape){
+	mShape.reset(shape->MakeCopy().release());
+	mShapeToWorldCoords.reset(shape->MakeCopy().release());
 }
 
 bool CollisionBox::operator < (const CollisionBox &rhs) const{
@@ -21,18 +29,41 @@ bool CollisionBox::operator < (const CollisionBox &rhs) const{
     return true;
 }
 
+const Shape* CollisionBox::GetWorldCoord(){
+	return mShapeToWorldCoords.get();
+}
+
+void CollisionBox::UpdateWorldCoord(){
+	auto wPos = mPos->GetPositionWorld();
+	mShapeToWorldCoords->x = mShape->x + wPos.x;
+	mShapeToWorldCoords->y = mShape->y + wPos.y;
+}
+
 CollisionResponse CollisionBox::Collides(const CollisionBox* box){
-	auto translationValue1 = mPos->GetPositionWorld();
-	auto translationValue2 = box->mPos->GetPositionWorld();
-
-	this-> mShape->Translate(translationValue1);
-	box->  mShape->Translate(translationValue2);
-
-	auto result = mShape->Contains(box->mShape.get());
-
-	this-> mShape->Translate(translationValue1 * -1.0f);
-	box->  mShape->Translate(translationValue2 * -1.0f);
-
+	auto result = mShapeToWorldCoords->Contains(box->mShapeToWorldCoords.get());
 	return result;
 }
 
+void CollisionBox::Activate(){
+	mActive = true;
+}
+
+void CollisionBox::Deactivate(){
+	mActive = false;
+}
+
+bool CollisionBox::IsActive() const{
+	return mActive;
+}
+
+void CollisionBox::SetFlags(uint8_t flags){
+	mFlags = flags;
+}
+
+uint8_t CollisionBox::Flags(){
+	return mFlags;
+}
+
+unsigned int CollisionBox::GetId(){
+	return mId;
+}
