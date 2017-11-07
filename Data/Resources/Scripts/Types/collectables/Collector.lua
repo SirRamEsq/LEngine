b17ebbc88cbde82f8b03908ea956dbed353d4588
SirRamESQ <gmrl95@gmail.com>
2017-11-07 15:47:11 -0500

    Add Collector type

---
+++ Data/Resources/Scripts/Types/collectables/Collector.lua
@@ -0,0 +1,21 @@
+local Collector = {}
+result, collectableFlags = pcall(loadfile(commonPath .. "/collectableFlags.lua", _ENV))
+
+function Collector.new(base)
+	local collector = base or {}
+	collector.collection = {}
+
+	function Collector.Collect(type, itemEID)
+		if collector.collection[type] == nil then
+			collector.collection[type] = 0
+		end
+		collector.collection[type] = collector.collection[type] + 1
+
+		CPP.interface:EntityDelete(itemEID)
+	end
+
+
+	return collector;
+end
+
+return Collector.new
