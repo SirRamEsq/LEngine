1f8f1f7fea3aebc016c7167e51a21b802dfe9ba6
SirRamESQ <gmrl95@gmail.com>
2017-11-12 16:45:53 -0500

    Add Icons folder and tsx

---
+++ Data/Resources/Maps/TileSets/icons.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="icons" tilewidth="18" tileheight="32" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="18" height="32" source="../Icons/louie.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../Icons/whiteGrunt.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../Icons/yellowGrunt.png"/>
+ </tile>
+</tileset>
