0c882520f4cdc2210e099ad9dd4402a06d4f17b1
SirRamESQ <gmrl95@gmail.com>
2017-11-21 10:52:24 -0500

    Add Prefab Unit Test

--- Source/Engine/Resources/RSC_Prefab.cpp
+++ Source/Engine/Resources/RSC_Prefab.cpp
@@ -16,26 +16,32 @@ std::unique_ptr<RSC_Prefab> RSC_Prefab::LoadFromXML(const char *dat,
   TiledMapProperties finalProperties;
   std::string XML = std::string(dat, size);
 
-  using namespace rapidxml;
-  xml_document<> doc;  // character type defaults to char
-  doc.parse<0>((char *)(XML.c_str()));
+  try {
+    using namespace rapidxml;
+    xml_document<> doc;  // character type defaults to char
+    doc.parse<0>((char *)(XML.c_str()));
 
-  // Find Specific Node
-  xml_node<> *node = doc.first_node("prefab");
-  xml_node<> *nodeScripts = node->first_node("scripts");
-  xml_node<> *nodeProperties = node->first_node("properties");
+    // Find Specific Node
+    xml_node<> *node = doc.first_node("prefab");
+    xml_node<> *nodeScripts = node->first_node("scripts");
+    xml_node<> *nodeProperties = node->first_node("properties");
 
-  if (nodeProperties != NULL) {
-    PropertyMap properties;
-    TMXLoadProperties(nodeProperties, properties);
-    finalProperties = TiledMapProperties(&properties);
-  }
-  if (nodeScripts != NULL) {
-    for (auto script = nodeScripts->first_node("script"); script != NULL;
-         script = script->next_sibling()) {
-      auto scriptName = script->first_attribute("name")->value();
-      finalScripts.push_back(scriptName);
+    if (nodeProperties != NULL) {
+      PropertyMap properties;
+      TMXLoadProperties(nodeProperties, properties);
+      finalProperties = TiledMapProperties(&properties);
     }
+
+    if (nodeScripts != NULL) {
+      for (auto script = nodeScripts->first_node("script"); script != NULL;
+           script = script->next_sibling()) {
+        auto scriptName = script->first_attribute("name")->value();
+        finalScripts.push_back(scriptName);
+      }
+    }
+
+  } catch (rapidxml::parse_error &e) {
+    LOG_ERROR(e.what());
   }
 
   return std::make_unique<RSC_Prefab>(finalScripts, &finalProperties);
